ea47691a2ebf2e5835bc4293a82a1d70c7150799
Eric Oliveira <ericpatrick15@gmail.com>
2023-09-07 08:52:18 -0300

    Dtmoney completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { Model, createServer } from 'miragejs';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const mockModels = {
+  transaction: Model.extend({
+    id: 1,
+    title: ""
+  })
+}
+
+const mockFactories = {}
+
+createServer<typeof mockModels, typeof mockFactories>({
+  models: mockModels,
+
+  seeds: (server) => {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00")
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = "api";
+    
+    this.get("/transactions", (schema) => schema.all("transaction"));
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      data.createdAt = new Date();
+
+      return schema.create("transaction", data);
+    });
+  },
+});
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
